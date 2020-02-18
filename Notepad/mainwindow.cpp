d5188b505ead373c349caa2f725abeca301b614e
vinicius <viniciusdpalves@gmail.com>
2020-02-18 19:42:38 -0300

    Simple method "New" add

---
+++ Notepad/mainwindow.cpp
@@ -0,0 +1,23 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setCentralWidget(ui->textEdit);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_actionNew_triggered()
+{
+    ui->textEdit->clear();
+    Local_file="";
+    ui->textEdit->setFocus();
+}
